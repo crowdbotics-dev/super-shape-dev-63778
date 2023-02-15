398604982ec65652139feb24d990277a2ab7ece0
Crowdbotics <engineering@crowdbotics.com>
2023-02-15 08:07:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/super_shape_dev_63778/MainActivity.java
@@ -0,0 +1,15 @@
+package com.super_shape_dev_63778;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "super_shape_dev_63778";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-shape-dev-63778",
+  url: "https://super-shape-dev-63778.botics.co",
+  api: "https://super-shape-dev-63778.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
